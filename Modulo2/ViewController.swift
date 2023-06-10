8f4ebb3594fdabadf4507a25c03f0054946eca26
Moises Garduño Reyes <moises.garduno.r@gmail.com>
2023-06-10 09:18:08 -0600

    Initial Commit

---
+++ Modulo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modulo2
+//
+//  Created by Moisés  Garduño Reyes on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
