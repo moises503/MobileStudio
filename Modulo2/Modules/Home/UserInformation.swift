675550db7508c3f734b685e93b6f8ae24ac6ea62
Moises Garduño Reyes <moises.garduno.r@gmail.com>
2023-07-07 22:01:51 -0600

    This commit adds tableview example and routing example

---
+++ Modulo2/Modules/Home/UserInformation.swift
@@ -0,0 +1,13 @@
+//
+//  UserInformation.swift
+//  Modulo2
+//
+//  Created by Moisés  Garduño Reyes on 24/06/23.
+//
+
+import Foundation
+
+struct UserInformation {
+    var title: String
+    var content: String
+}
